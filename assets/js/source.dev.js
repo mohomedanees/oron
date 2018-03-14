ffed45b7be3b4d8a0f5407394b596fc29811ed6a
mohomedanees <anees@thesmallaxxe.com>
2018-03-14 22:35:39 +0530

    Script changes.

--- assets/js/source.dev.js
+++ assets/js/source.dev.js
@@ -177,25 +177,27 @@
 
 	function () {
 	  $(".quantity-control").on("click", function () {
-
 	    var $button = $(this);
 	    var oldValue = $button.parent().parent().find("input.quantity").val();
 
-	    if ($button.hasClass('increase')) {
-	      var newVal = parseFloat(oldValue) + 1;
+	    if (!$button.parent().parent().find("input.quantity").prop("disabled")) {
+	      if ($button.hasClass('increase')) {
+	        var newVal = parseFloat(oldValue) + 1;
 
-	      if (oldValue > 9) {
-	        return false;
-	      }
-	    } else {
-	      if (oldValue > 1) {
-	        var newVal = parseFloat(oldValue) - 1;
+	        if (oldValue > 1000) {
+
+	          return false;
+	        }
 	      } else {
-	        newVal = 1;
+	        if (oldValue > 1) {
+	          var newVal = parseFloat(oldValue) - 1;
+	        } else {
+	          newVal = 1;
+	        }
 	      }
-	    }
 
-	    $button.parent().parent().find("input.quantity").val(newVal);
+	      $button.parent().parent().find("input.quantity").val(newVal);
+	    }
 	  });
 	};
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
--- src/js/modules/numberIncrement.js
+++ src/js/modules/numberIncrement.js
@@ -3,25 +3,27 @@
 */
 
 export default function() {
-  $(".quantity-control").on("click", function() {
-
+  $(".quantity-control").on("click", function () {
     var $button = $(this);
     var oldValue = $button.parent().parent().find("input.quantity").val();
+    
+    if (!$button.parent().parent().find("input.quantity").prop("disabled")) {
+      if ($button.hasClass('increase')) {
+        var newVal = parseFloat(oldValue) + 1;
 
-    if ($button.hasClass('increase')) {
-      var newVal = parseFloat(oldValue) + 1;
+        if (oldValue > 1000) {
 
-      if (oldValue > 9) {
-        return false;
-      }
-    } else {
-      if (oldValue > 1) {
-        var newVal = parseFloat(oldValue) - 1;
+          return false;
+        }
       } else {
-        newVal = 1;
+        if (oldValue > 1) {
+          var newVal = parseFloat(oldValue) - 1;
+        } else {
+          newVal = 1;
+        }
       }
-    }
 
-    $button.parent().parent().find("input.quantity").val(newVal);
+      $button.parent().parent().find("input.quantity").val(newVal);
+    }
   });
 }
